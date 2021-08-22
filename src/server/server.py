34f4a1d777aec54769bc93556063f2bedbb6c8e2
Иван Бунеев <buneev_ivan_r@mail.ru>
2021-08-22 18:24:16 +0300

    Реализовал запуск парсинга риа-новости

--- src/server/server.py
+++ src/server/server.py
@@ -1,32 +1,45 @@
 import asyncio
 from aiohttp import web
 import logging
+from settings import * 
+import os
 
-INTERVAL_SECS = 1
 
-async def parse_site(request):
+async def parse(request):
+    ''' запуск процесса парсинга '''
     jo = await request.json()
-    cmd = f"cd /code/src && scrapy crawl {jo['spider']} --logfile=/log/{jo['spider']}.log"
-    cmd = cmd + f" -a debug={jo.get('debug', False)}"
-    pid = await run(cmd)
-    logging.info(f'Run web scraping success, PID: {pid}\nCMD: {cmd}')
-    return web.json_response(jo)
+    if DEBUG:
+        path = 'cd ../ && '
+    else:
+        path = 'cd /code/src && '
 
-async def run(cmd):
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
+    sp_name = jo.get('spider_name', '')
+    cmd = f"{path}scrapy crawl {sp_name} -o data/{sp_name}.json --logfile=data/log/{sp_name}.log"
 
-    stdout, stderr = await proc.communicate()
+    proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    
+    # logger.info(f'{cmd!r} Exited with {proc.returncode}')
+    # stdout, stderr = await proc.communicate()
+    # text = ''
+    # status = ''
+    # if stdout:
+    #     text = stdout.decode()
+    #     logger.info(f"[stdout] {text}")
+    #     status = 'success'
+    # if stderr:
+    #     text = stderr.decode()
+    #     logger.error(f"[stderr] {text}")
+    #     status = 'error'
+    # return web.json_response({'success': status, 'text': text})
+    return web.json_response({'success': 'true'})
 
 
-# async def main():
-logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s', level = logging.DEBUG)
-app = web.Application()
-app.add_routes([
-    web.get('/parse_site', parse_site),
-])
-web.run_app(app, port=5858)
-# return app
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes([
+        web.post('/run_parse', parse)
+    ])
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    logger = logging.getLogger("[scrapy]")
+    web.run_app(app, port=5858)
 
